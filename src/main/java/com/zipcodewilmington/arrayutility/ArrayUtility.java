436c9a6c51be54af96a7e99dd6f42b2638457b5e
lena <alena.litouka@gmail.com>
2021-03-29 18:40:53 -0400

    genetics

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,32 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.ArrayList;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T>{
+    ArrayList<T> list;
+
+    public ArrayUtility() {}
+
+    public ArrayUtility(T[] inputArray){
+
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+    }
+
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+    }
+
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+    }
+
+    public T[] removeValue(T valueToRemove) {
+    }
+
+
 }
