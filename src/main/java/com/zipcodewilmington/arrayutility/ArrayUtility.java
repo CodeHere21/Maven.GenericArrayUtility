59e8afb4b64bf111ddbe7a094194dc2201861916
lena <alena.litouka@gmail.com>
2021-03-29 21:57:41 -0400

    finished

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,31 +1,75 @@
 package com.zipcodewilmington.arrayutility;
 
-import java.util.ArrayList;
+import java.lang.reflect.Array;
+import java.util.*;
 
 /**
  * Created by leon on 3/6/18.
  */
 public class ArrayUtility<T>{
-    ArrayList<T> list;
-
+    T[] inputArray;
     public ArrayUtility() {}
 
     public ArrayUtility(T[] inputArray){
-
+        this.inputArray=inputArray;
     }
 
     public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        int count=0;
+
+        for (int i=0; i< inputArray.length; i++){
+            if (inputArray[i].equals(valueToEvaluate)){
+                count++;
+            }
+        }
+        for (int i=0; i< arrayToMerge.length;i++){
+            if (arrayToMerge[i].equals(valueToEvaluate)){
+                count++;
+            }
+        }
+        return count;
     }
 
 
     public T getMostCommonFromMerge(T[] arrayToMerge) {
+        T mostCommon = null;
+
+        for(int i=0; i< inputArray.length; i++){
+            for(int j=0; j< arrayToMerge.length;j++){
+                if(inputArray[i] == arrayToMerge[j]) {
+                        mostCommon = inputArray[i];
+                }
+            }
+        }
+        return mostCommon;
     }
 
 
     public Integer getNumberOfOccurrences(T valueToEvaluate) {
+        Integer count = 0;
+        for(int i=0; i< inputArray.length;i++){
+        if(inputArray[i].equals(valueToEvaluate)){
+            count++;
+        }
+        }
+        return count;
     }
 
     public T[] removeValue(T valueToRemove) {
+        T[] result;
+        Integer count = 0;
+        Integer removeCount = getNumberOfOccurrences(valueToRemove);
+        Integer newSize=inputArray.length-removeCount;
+
+        result= Arrays.copyOf(inputArray, newSize);
+        for(int i=0; i<inputArray.length;i++){
+            if(inputArray[i] != valueToRemove){
+                result[count]=inputArray[i];
+                count++;
+            }
+        }
+
+        return result;
     }
 
 
